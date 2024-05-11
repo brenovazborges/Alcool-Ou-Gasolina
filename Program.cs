c4d6616df8c9344a130f6d640a693916730023c7
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-10 22:39:32 -0300

    ATIVIDADE CONCLUIDA

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿double gasolina, comparacao, alcool;
+
+Console.WriteLine(">>>>ATIVIDADE 'ALCOOL E GASOLINA'<<<<\n");
+
+Console.Write("Digite o preço do álcool:");
+alcool = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o preço da gasolina:");
+gasolina = Convert.ToDouble(Console.ReadLine());
+
+comparacao = (alcool / gasolina) * 100;
+
+Console.WriteLine($"O preço do alcool corresponde a {comparacao}% do preço da gasolina.");
+
+if (comparacao <= 73)
+
+  {
+     Console.WriteLine("Recomendação: Abasteça com ALCOOL.");
+  }
+
+else
+
+  {
+   Console.WriteLine( "Recomendação: Abasteça com GASOLINA.");
+  }
